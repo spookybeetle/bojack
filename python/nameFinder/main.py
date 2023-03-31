2f0c964f73cd224932f0c44cc933c58600a3de5d
spookybeetle <krw5520@psu.edu>
2023-03-31 10:55:29 -0400

    Update nlp nameFinder

--- python/nameFinder/main.py
+++ python/nameFinder/main.py
@@ -1,87 +1,135 @@
-import spacy
-from collections import Counter
+# STAGE 2: LET'S WORK WITH SELECT DATA FROM XML, PULLED WITH XPATH
+# pip install saxonche
+# The library above lets you read and pull data with XPath
 import os
-nlp = spacy.cli.download("en_core_web_lg")
-# Uncomment this line if you need the language model.
-# If you already have it, comment it ou.
-# Let's try the different spaCy language models for this. We can compare _lg with _md or _sm
-nlp = spacy.load("en_core_web_lg")
+import spacy
+import re as regex
+from saxonche import PySaxonProcessor
+# ebb: The line above imports the PySaxonProcessor from SaxonCHE (free "home edition")
+# for work with XPath
 
-workingDir = os.getcwd()
-print("current working directory: " + workingDir)
-# os.listdir lists files and folders inside a path:
-insideDir = os.listdir(workingDir)
-print("inside this directory are the following files AND directories: " + str(insideDir))
 
-CollPath = os.path.join(workingDir, 'xmlFile')
-print(CollPath)
+# nlp = spacy.cli.download("en_core_web_lg")
+nlp = spacy.load('en_core_web_lg')
+# ebb: In the line above I'm loading one of the spaCy language models: use either _md or _lg.
+# If you change versions, you need to uncomment the line above and import it, and it can take
+# a little while to finish importing.
+# LOTR PROJECT NOTE: When I ran this with the _md language model, Frodo was being tagged as a PRODUCT.
+# The _lg model seemed more accurate (Frodo as PERSON)
 
+# workingDir = os.getcwd()
+# print("source-txt" + workingDir)
+#
+# insideDir = os.listdir(workingDir)
+# print(str(insideDir))
+#
+# CollPath = os.path.join(workingDir, '../source-xml')
+# print(CollPath)
+###############################################################################
+################################################################################
+# 1. ebb: CollPath can also simply be defined more simply as a relative path
+# defined from this Python file's location, like this, because you climb up one directory
+# and then down into your source XML files:
+##################################################################################
+CollPath = '../../xmlFile'
+
+
+# 3. Here, the function imports each individual file, one at a time
+# (received from the for-loop below.
 def readTextFiles(filepath):
-    with open(filepath, 'r', encoding='utf8') as f:
-        readFile = f.read()
-        # print(readFile)
-        stringFile = str(readFile)
-        # lengthFile = len(readFile)
-        # print(lengthFile)
-        tokens = nlp(stringFile)
+    # with open(filepath, 'r', encoding='utf8') as f:
+    with PySaxonProcessor(license=False) as proc:
+        xml = open(filepath, encoding='utf-8').read()
+        # ebb: Here we changed to the Saxon processor to read files with XPath.
+        # From here on, we change how we formulate the string that Python will send to NLP.
+        xp = proc.new_xpath_processor()
+        node = proc.parse_xml(xml_text=xml)
+        xp.set_context(xdm_item=node)
+
+        xpath = xp.evaluate('//script//line ! normalize-space() => string-join()')
+        # ebb: Let's get the string() value of all the <p> elements that are descendants of <book>.
+        # The XPath function normalize-space() gets the string value and removes extra spaces.
+        # That way we avoid the prologue, preface material.
+        # I'm sending the whole thing to string-join() to bundle it together as one string
+        # instead of a new string for every <p> element.
+        # string = xpath.__str__()
+        string = str(xpath)
+
+        # ebb: Using REGEX to remove element tags for the moment so they don't get involved in the NLP.
+        # elementsRemoved = regex.sub('<.+?>', '', xpath)
+        # ebb: Now I don't have to remove elements because I pulled a string value out of my XML.
+        # Should we remove the single quotation marks? Not sure. If we do, uncomment the next line:
+        # cleanedUp = string.replace("'", '')
+        # ebb: I noticed some sentences didn't have a space between end punctuation and the first word of the next sentence.
+        cleanedUp = regex.sub("(\.)([A-Z']])", "\1 \2", string)
+        # NOT REALLY DOING THIS, but just for illustration if we want to continue refining the input in multiple stages:
+        # cleanedUp = regex.sub("(\d)", "NUMBER", cleanedUp)
+        # print(cleanedUp)
+        tokens = nlp(cleanedUp)
         # print(tokens)
         listEntities = entitycollector(tokens)
-        print(listEntities)
-        # cardinal_freq = Counter(listCardinals)
-        # topTen = cardinal_freq.most_common(10)
-        # print(topTen)
+        # ebb: The line above sends our nlp tokens to the named entity collector function.
+        # THIS current function will receive and print a simple form of their output in the next line.
+        # print(listEntities)
+        return(listEntities)
 
+#########################################################################################
+# ebb: NEXT AFTER RETURNING ALL THE ENTITIES
+# Remove duplicates (get the distinct values of the list of entities (DONE! below)
+# Output this information in a useful way (TO DO)
+# Map it back into the XML files (TO DO)
+##########################################################################################
 
-
+# 4. ebb: The function below returns a simple list of named entities.
+# But on the way, we're printing out as much we can from spaCy's classification of named entities:
 def entitycollector(tokens):
-    entities = []
-    for entity in tokens.ents:
-        # if entity.label_ == "CARDINAL":
-        print(entity.text, entity.label_, spacy.explain(entity.label_))
-        entities.append(entity.text)
-    return entities
+    with open('output.txt', 'w') as f:
+        entities = []
+        for entity in tokens.ents:
+        # if entity.label_ == "NORP" or entity.label_ == "LOC" or entity.label_=="GPE":
+        # ebb: The line helps experiment with different spaCy named entity classifiers, in combination if you like:
+        # When using it, remember to indent the next lines for the for loop.
+            # print(entity.text, entity.label_, spacy.explain(entity.label_))
+            entityInfo = [entity.text, entity.label_, spacy.explain(entity.label_)]
+            stringify = str(entityInfo)
+            print(stringify)
+            f.write(stringify)
+            f.write('\n')
+            entities.append(entity.text)
+        print(f"{entities=}")
+        return entities
+    # ebb: Keep the return line in position at same indentation level as the definition of the entities variable.
 
 
-for file in os.listdir(CollPath):
-    if file.endswith(".xml"):
-        filepath = f"{CollPath}/{file}"
-        print(filepath)
-        readTextFiles(filepath)
+# 2. ebb: The for loop below is working with your CollPath, and going through each file inside,
+# and sending it up to readTextFiles, where the nlp processing will happen.
+def assembleAllNames(CollPath):
+    AllNames = []
+    for file in os.listdir(CollPath):
+        if file.endswith(".xml"):
+            filepath = f"{CollPath}/{file}"
+            # print(filepath)
+            # print(readTextFiles(filepath))
+            eachFileList = readTextFiles(filepath)
+            print(eachFileList)
+            AllNames.append(eachFileList)
+    # print(AllNames)
+    # print(len(AllNames))
+    # ebb: Okay. Now let's return distinct values of this giant list!
+    # ebb: NOTE: AllNames is a list of 3 nested lists (one for each book of LOTR, or each turn of the for loop.
+    # We need to flatten the nested list before we can properly get distinct values from it.
+    # We'll use a list comprehension for that.
+    flatList = [element for innerList in AllNames for element in innerList]
+    # ebb: This strange looking thing in the line above is a "list comprehension". It unpacks elements from the 3 inner lists
+    # and organizes them out on the same level.
+    distinctNames = set(flatList)
+    # ebb: Converting a list to a set() removes duplicates from the list. Yay.
+    print(f"{distinctNames=}")
+    print('AllNames Count: ' + str(len(AllNames)) + ' : ' + 'Distinct Names Count: ' + str(len(distinctNames)) + ' : ' + 'flatList Count ' + str(len(flatList)))
+    with open('distNames.txt', 'w') as f:
+        f.write(str(distinctNames))
+    return distinctNames
 
-
-
-
-
-# print(listCardinals)
-# cardinal_freq = Counter(listCardinals)
-# topTen = cardinal_freq.most_common(10)
-# print(topTen)
-
-# grimm = open('grimm.txt', 'r', encoding='utf8')
-# grimmDoc = grimm.read()
-# grimmNLP = nlp(grimmDoc)
-# grimmmSentences = grimmNLP.sents
-
-# def sentenceLengths(sentences):
-#     lengths = []
-#     for s in sentences:
-#         length = len(s.text)
-#         lengths.append(length)
-#     return sorted(lengths)
-
-
-# grimmLengths = sentenceLengths(grimmmSentences)
-# # print(grimmLengths)
-# maxVal = max(grimmLengths)
-# minVal = min(grimmLengths)
-# print('The shortest sentence is ' + str(minVal) + ' characters long.')
-# print('The longest sentence is ' + str(maxVal) + ' characters long.')
-
-# for sentence in grimmNLP.sents:
-# #    print(sentence.text)
-#     length = len(sentence.text)
-#     if length == minVal:
-#         print("The shortest sentence is: " + sentence.text)
-#     if len(sentence.text) == maxVal:
-#         print('The longest sentence is: ' + sentence.text + ' :' + str(maxVal) + 'characters')
-
+assembleAllNames(CollPath)
+# ebb: The functions are all initiated here now.
+# This just delivers the collection path up to the first function in the sequence.
